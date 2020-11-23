c9a853153a2c729918ee59f5e0e137db03e8d2e1
Ghidyon <gideonakunana@gmail.com>
2020-11-23 14:41:41 +0100

    added search filter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const search = document.querySelector('#search');
 const loader = document.querySelector('.loader');
+const err = document.querySelector('.error');
 
 window.onload = () => {
     console.log("Hello")
@@ -11,7 +12,6 @@ const loadNumbers = async () => {
     try {
         const response = await fetch('../numbers.json');
         const stations = await response.json();
-        console.log(stations);
 
         let output = "";
         stations.forEach(station => {
@@ -33,12 +33,49 @@ const loadNumbers = async () => {
     catch (error) {
         const errorMessage = `<img src="images/icon-error.svg">
                                 <span>Failed to fetch emergency numbers, please check your internet connection.</span>`;
-        // displayError(errorMessage);
+        displayError(errorMessage);
     }
 }
 
+const getNumbers = async searchValue => {
+    const response = await fetch('../numbers.json');
+    const stations = await response.json();
+
+    // Get matches to current text input
+    const stationMatches = stations.filter(station => {
+        const regex = new RegExp(`${searchValue}`, 'gi'); // regex to match stations with search value
+        return station.name.match(regex);
+    })
+
+    let output = ``;
+    stationMatches.forEach(station => {
+        const { name, number } = station;
+        output += `<div class="station">
+                        <div>
+                            <a href="tel:${number}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon-phone" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                                </svg>
+                            </a>
+                        </div>
+                        <p>${name}</p>
+                    </div>`;
+    })
+    displayNumbers(output);
+}
+
+search.addEventListener('input', e => getNumbers(search.value));
+
+// Function for UI
 
 const displayNumbers = (display) => {
     document.querySelector('.stations').innerHTML = display;
     loader.style.visibility = "hidden";
+}
+
+const displayError = (error) => {
+    loader.style.visibility = "hidden";
+    err.innerHTML = error;
+    err.style.display = "flex";
 }
